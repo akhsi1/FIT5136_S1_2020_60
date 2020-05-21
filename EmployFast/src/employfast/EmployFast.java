759c2f0c17713551ca136475254a2ad72e93d923
Kang Hsing <akhsi1@student.monash.edu>
2020-05-21 12:17:09 +1000

    Initial project upload
    NETBEANS Project file is created with a few classes initialized

---
+++ EmployFast/src/employfast/EmployFast.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employfast;
+
+/**
+ *
+ * @author akhsi
+ */
+public class EmployFast {
+
+    private User[] userList;
+    private SelectionCriteria[] selectionCriteriaList;
+    private Mission[] missionList;
+    private SpaceShuttle[] shuttleList;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.displayLogin();
+    }
+   
+    
+    
+}
