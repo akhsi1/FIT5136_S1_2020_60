2dc069c95e01c1b0126bf1348624d9dd74477478
Alexander <rjii0001@student.monash.edu>
2020-05-25 11:12:03 +0800

    change user class

--- EmployFast/src/employfast/User.java
+++ EmployFast/src/employfast/User.java
@@ -10,5 +10,51 @@ package employfast;
  * @author akhsi
  */
 class User {
+    private String userName;
+    private String userId;
+    private String password;
     
+    public User()
+    {
+        userName = "";
+        userId = "";
+        password = "";
+    }
+    
+    public User(String userName,String userId,String password)
+    {
+        this.userName = userName;
+        this.userId = userId;
+        this.password = password;
+    }
+    
+    public String getUserName()
+    {
+        return userName;
+    }
+    
+    public String getUserId()
+    {
+        return userId;
+    }
+    
+    public String getPassword()
+    {
+        return password;
+    }
+    
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+    
+    public void setUserId(String userId)
+    {
+        this.userId = userId;
+    }
+    
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
 }
