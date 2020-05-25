be617989f4098789ee68ff4104dcb9c12ae6657d
Shonik Raj <sraj0024@student.monash.edu>
2020-05-25 15:14:57 +1000

    working on spaceshuttle

--- EmployFast/src/employfast/SpaceShuttle.java
+++ EmployFast/src/employfast/SpaceShuttle.java
@@ -11,4 +11,60 @@ package employfast;
  */
 class SpaceShuttle {
     
+    private String shuttleName;
+    private String shuttleManuYear;
+    private String shuttleFuelCap;
+    private String shuttlePassCap;
+    private String shuttleCargoCap;
+    private String shuttleTrvlSpeed;
+    
+    public SpaceShuttle()
+    {
+        shuttleName = "";
+        shuttleManuYear = "";
+        shuttleFuelCap = "";
+        shuttlePassCap = "";
+        shuttleCargoCap = "";
+        shuttleTrvlSpeed = "";
+    }
+
+    public SpaceShuttle(String shuttleName,int shuttleManuYear, String shuttleFuelCap, int shuttlePassCap, String shuttleCargoCap, float shuttleTrvlSpeed)
+    {
+        this.shuttleName = shuttleName;
+        this.shuttleManuYear = shuttleManuYear;
+        this.shuttleFuelCap = shuttleFuelCap;
+        this.shuttlePassCap = shuttlePassCap;
+        this.shuttleCargoCap = shuttleCargoCap;
+        this.shuttleTrvlSpeed = shuttleTrvlSpeed;
+    }
+
+    public String getShuttleName()
+    {
+        return shuttleName;
+    }
+
+    public String getUserId()
+    {
+        return userId;
+    }
+
+        public String getPassword()
+        {
+            return password;
+        }
+
+        public void setUserName(String userName)
+        {
+            this.userName = userName;
+        }
+
+        public void setUserId(String userId)
+        {
+            this.userId = userId;
+        }
+
+        public void setPassword(String password)
+        {
+            this.password = password;
+        }
 }
